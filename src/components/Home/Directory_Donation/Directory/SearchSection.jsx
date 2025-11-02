e0d413e61cb22d73fd50763d8ee8e1e2339048fb
AhmadShopnil <ahmadshopnil75@gmail.com>
2025-11-02 15:02:00 +0600

    fix gradient border

--- src/components/Home/Directory_Donation/Directory/SearchSection.jsx
+++ src/components/Home/Directory_Donation/Directory/SearchSection.jsx
@@ -4,42 +4,39 @@ import { Search, MapPin } from "lucide-react";
 
 export default function SearchSection() {
   return (
-    <div className="w-full flex flex-col md:flex-row items-center justify-start gap-4 flex-wrap">
+    <div className="flex items-center gap-3">
       {/* Search Input */}
-      <div className="relative w-full sm:w-[280px] md:w-[240px] ">
+      <div className="relative flex-1 max-w-xs">
         <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
         <input
           type="text"
           placeholder="Find a business"
-          className="w-full h-[56px] pl-10 pr-4 py-3 border border-green-900 rounded-full focus:outline-none placeholder-gray-400"
+          className="w-full pl-10 pr-4 py-3 border border-green-900 rounded-full focus:outline-none placeholder-gray-400"
         />
       </div>
 
       {/* Location Input */}
-      <div className="relative w-full sm:w-[280px] md:w-[240px]">
+      <div className="relative flex-1 max-w-xs">
         <MapPin className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
         <input
           type="text"
           placeholder="Dhaka,1212"
-          className="w-full h-[56px] pl-10 pr-4 py-3 border border-green-900 rounded-full focus:outline-none placeholder-gray-400"
+          className="w-full pl-10 pr-4 py-3 border border-green-900 rounded-full focus:outline-none placeholder-gray-400"
         />
       </div>
-
-      {/* Another Input (Optional) */}
-      <div className="relative w-full sm:w-[280px] md:w-[240px]">
+        {/* Location Input */}
+      <div className="relative flex-1 max-w-xs">
         <MapPin className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
         <input
           type="text"
-          placeholder="Another filter"
-          className="w-full h-[56px] pl-10 pr-4 py-3 border border-green-900 rounded-full focus:outline-none placeholder-gray-400"
+          placeholder="Dhaka,1212"
+          className="w-full pl-10 pr-4 py-3 border border-green-900 rounded-full focus:outline-none placeholder-gray-400"
         />
       </div>
 
       {/* Find Button */}
-      <button
-        className="h-[56px] bg-[#F7BA2A] hover:bg-[#f8c645] text-[#00401A] font-semibold px-10 py-3 rounded-full 
-        shadow-md transition text-lg w-full sm:w-auto"
-      >
+      <button className="cursor-pointer bg-[#F7BA2A] hover:bg-[#F7BA2A] text-[#00401A] font-semibold px-9 py-3 rounded-full 
+      shadow-md transition text-lg">
         Find
       </button>
     </div>
