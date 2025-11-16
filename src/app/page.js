01b5b879496079686e080c8023cb49fffdca24c5
AhmadShopnil <ahmadshopnil75@gmail.com>
2025-11-16 14:29:01 +0600

    delete dua home

--- src/app/page.js
+++ src/app/page.js
@@ -49,7 +49,7 @@ export default async function Home() {
       <OfferServices/>
        <DictionarySection/>
       <DirecToryDonation/>
-      <DuaSection/>
+      {/* <DuaSection/> */}
       <GallerySection/>
       <IslamicBooks/>
      </div>
